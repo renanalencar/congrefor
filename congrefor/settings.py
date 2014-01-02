46af045c378c6b01d4666553bbf31fdccaaa76b7
renanalencar <services@renanalencar.eng.br>
2014-01-01 23:33:25 -0300

    Changed 'console' to 'smtp' on EMAIL_BACKEND

--- congrefor/settings.py
+++ congrefor/settings.py
@@ -217,7 +217,7 @@ AUTHENTICATION_BACKENDS = [
 
 LOGIN_URL = "/account/login/"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 #EMAIL_USE_TLS = True 
 #EMAIL_HOST = 'smtp.gmail.com' 
 #EMAIL_PORT = 587 
