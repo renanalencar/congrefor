0c75ab9351fe33140d6b7125420a5b9437301d83
renanalencar <services@renanalencar.eng.br>
2014-01-15 10:33:08 -0300

    Added payment boolean field to account app.

---
+++ congrefor/account/models.py
@@ -0,0 +1,38 @@
+#import datetime
+#import operator
+#import urllib
+
+#from django.core.mail import send_mail
+#from django.core.urlresolvers import reverse
+#from django.db import models, transaction
+#from django.db.models import Q
+#from django.db.models.signals import post_save
+#from django.dispatch import receiver
+#from django.template.loader import render_to_string
+#from django.utils import timezone, translation
+#from django.utils.translation import gettext_lazy as _
+
+#from django.contrib.auth.models import User, AnonymousUser
+#from django.contrib.sites.models import Site
+
+#import pytz
+
+#from account import signals
+#from account.conf import settings
+#from account.fields import TimeZoneField
+#from account.managers import EmailAddressManager, EmailConfirmationManager
+#from account.signals import signup_code_sent, signup_code_used
+#from account.utils import random_token
+
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+from account.models import Account
+
+from django.utils.translation import ugettext_lazy as _
+
+
+class Account(Account):
+    
+    payment = models.BooleanField(_("payment"), default=False)
