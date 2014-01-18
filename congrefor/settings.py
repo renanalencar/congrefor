a8ee9d484a173d6e4fa63c916100a322bae297ec
renanalencar <services@renanalencar.eng.br>
2014-01-18 01:33:12 -0300

    S3

--- congrefor/settings.py
+++ congrefor/settings.py
@@ -273,11 +273,11 @@ if not DEBUG:
  AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
  AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
  AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
- #STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
- DEFAULT_FILE_STORAGE = 'congrefor.s3utils.MediaRootS3BotoStorage'
- STATICFILES_STORAGE = 'congrefor.s3utils.StaticRootS3BotoStorage'
+ STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+ #STATICFILES_STORAGE = 'congrefor.s3utils.StaticRootS3BotoStorage'
  S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
  #STATIC_URL = S3_URL
- STATIC_URL = 'https://s3.amazonaws.com/congrefor/static/'
- ADMIN_MEDIA_PREFIX = 'https://s3.amazonaws.com/congrefor/static/admin/'
+ #STATIC_URL = 'https://s3.amazonaws.com/congrefor/static/'
+ #ADMIN_MEDIA_PREFIX = 'https://s3.amazonaws.com/congrefor/static/admin/'
  MEDIA_URL = 'https://s3.amazonaws.com/congrefor/media/'
