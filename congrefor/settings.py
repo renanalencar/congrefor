7e2e76f3b22d08a67b2c89c0d4013e06483a1692
renanalencar <services@renanalencar.eng.br>
2014-01-18 01:24:09 -0300

    S3

--- congrefor/settings.py
+++ congrefor/settings.py
@@ -274,8 +274,8 @@ if not DEBUG:
  AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
  AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
  #STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
- DEFAULT_FILE_STORAGE = 'myproject.s3utils.MediaRootS3BotoStorage'
- STATICFILES_STORAGE = 'myproject.s3utils.StaticRootS3BotoStorage'
+ DEFAULT_FILE_STORAGE = 'congrefor.s3utils.MediaRootS3BotoStorage'
+ STATICFILES_STORAGE = 'congrefor.s3utils.StaticRootS3BotoStorage'
  S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
  #STATIC_URL = S3_URL
  STATIC_URL = 'https://s3.amazonaws.com/congrefor/static/'
