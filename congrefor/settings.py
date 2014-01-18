6f9d7f53a70258a5621ef5ed3044dc6fc36a85e6
renanalencar <services@renanalencar.eng.br>
2014-01-18 00:52:50 -0300

    S3

--- congrefor/settings.py
+++ congrefor/settings.py
@@ -275,5 +275,5 @@ if not DEBUG:
  AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
  STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
  S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
- STATIC_URL = S3_URL
+ STATIC_URL = S3_URL + STATIC_ROOT
  MEDIA_URL = S3_URL + MEDIA_ROOT
