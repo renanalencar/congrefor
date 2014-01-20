845bf973a0b590312fba2588037c5d0fccdaf6f3
renanalencar <services@renanalencar.eng.br>
2014-01-20 12:35:03 -0300

    S3

--- congrefor/settings.py
+++ congrefor/settings.py
@@ -270,8 +270,8 @@ if not DEBUG:
  STATICFILES_STORAGE = 'congrefor.s3utils.StaticRootS3BotoStorage'
  DEFAULT_FILE_STORAGE = 'congrefor.s3utils.MediaRootS3BotoStorage'
  S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
- STATIC_DIRECTORY = '/static/'
- MEDIA_DIRECTORY = '/media/'
+ STATIC_DIRECTORY = 'static/'
+ MEDIA_DIRECTORY = 'media/'
  STATIC_URL = S3_URL + STATIC_DIRECTORY
  MEDIA_URL = S3_URL + MEDIA_DIRECTORY
  ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
