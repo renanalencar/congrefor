62f7a23e66fc62b5d66d63b2ff2009caea909555
renanalencar <services@renanalencar.eng.br>
2014-01-20 12:18:04 -0300

    S3

--- congrefor/settings.py
+++ congrefor/settings.py
@@ -267,12 +267,12 @@ if not DEBUG:
  # necessary to fix manage.py collectstatic command to only upload changed files instead of all files.
  #AWS_PRELOAD_METADATA = True
  AWS_QUERYSTRING_AUTH = False
- STATICFILES_STORAGE = 'congrefor.s3utils.MediaRootS3BotoStorage'
- DEFAULT_FILE_STORAGE = 'congrefor.s3utils.StaticRootS3BotoStorage'
+ STATICFILES_STORAGE = 'congrefor.s3utils.StaticRootS3BotoStorage'
+ DEFAULT_FILE_STORAGE = 'congrefor.s3utils.MediaRootS3BotoStorage'
  S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
  STATIC_DIRECTORY = '/static/'
  MEDIA_DIRECTORY = '/media/'
  STATIC_URL = S3_URL + STATIC_ROOT
  MEDIA_URL = S3_URL + MEDIA_ROOT
- #ADMIN_MEDIA_PREFIX = S3_URL + 'admin/'
+ ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
  
